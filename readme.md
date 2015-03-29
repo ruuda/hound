d041a96d191c95cd234fb844eb4589aac267883d
Ruud van Asseldonk <dev@veniogames.com>
2015-03-29 14:53:35 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+Hound
+=====
+
+A WAV encoding library in Rust.
+
+Hound takes audio samples and writes a WAV file. The main motivation to write it
+was to test [Claxon][claxon], a FLAC decoding library written in Rust.
+
+Hound is licensed under the [GNU General Public License][gplv3].
+
+[claxon]: https://github.com/ruud-v-a/claxon
+[gplv3]:  https://www.gnu.org/licenses/gpl.html
+
+[![Build Status](https://travis-ci.org/ruud-v-a/hound.svg?branch=master)](https://travis-ci.org/ruud-v-a/hound)
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// Hound -- A WAV encoding library in Rust
+// Copyright (C) 2015 Ruud van Asseldonk
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Hound, a WAV encoding library.
+//!
+//! TODO: Add some examples here.
+
+#![warn(missing_docs)]
+#![allow(dead_code)] // TODO: Remove for v0.1
