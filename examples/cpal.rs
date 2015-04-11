929efa907976ee02dab5a847ab237a25cd3cfda7
Ruud van Asseldonk <dev@veniogames.com>
2015-04-11 14:31:44 +0200

    add rough cpal example
    Unfortuntely, cpal does not support my audio device settings yet, so this fails
    to play.

---
+++ examples/cpal.rs
@@ -0,0 +1,58 @@
+// Hound -- A WAV encoding and decoding library in Rust
+// Copyright (C) 2015 Ruud van Asseldonk
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This example shows how to play a wav file using the cpal crate.
+
+extern crate hound;
+extern crate cpal;
+
+use std::env;
+use std::fs;
+use std::io;
+use std::path;
+
+fn main() {
+    // Make a WavReader that reads the file provided as program argument.
+    let arg = env::args().nth(1).unwrap();
+    let fname = path::Path::new(&arg);
+    let input = fs::File::open(fname).unwrap();
+    let buf_reader = io::BufReader::new(input);
+    let mut reader = hound::WavReader::new(buf_reader).unwrap();
+    let spec = reader.spec().clone(); // TODO: by value might be better in this case.
+
+    // A voice in cpal is used for playback.
+    let mut voice = cpal::Voice::new();
+
+    let mut append_data = |voice: &mut cpal::Voice| {
+        let mut buffer: cpal::Buffer<i16> =
+            voice.append_data(spec.channels,
+                              cpal::SamplesRate(spec.sample_rate),
+                              1024); // TODO: Do I know the number of samples?
+        // Fill the cpal buffer with data from the wav file.
+        for (dest, src) in buffer.iter_mut().zip(reader.samples()) {
+            *dest = src.unwrap();
+        }
+        // TODO: check when reader is done, then exit.
+    };
+
+    // The voice must have some data before playing for the first time.
+    append_data(&mut voice);
+    voice.play();
+
+    // Then we keep providing new data until the end of the audio.
+    loop {
+        append_data(&mut voice); // TODO exit when done
+    }
+}
