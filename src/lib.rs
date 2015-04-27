e933329b134ecadb795cde2b9fb3431d47c09384
Ruud van Asseldonk <dev@veniogames.com>
2015-04-27 17:43:34 +0200

    simplify doc example

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,8 @@
 //! let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
 //! for t in (0 .. 44100).map(|x| x as f32 / 44100.0) {
 //!     let sample = (t * 440.0 * 2.0 * PI).sin();
-//!     let amplitude: i16 = i16::MAX;
-//!     writer.write_sample((sample * amplitude as f32) as i16).unwrap();
+//!     let amplitude = i16::MAX as f32;
+//!     writer.write_sample((sample * amplitude) as i16).unwrap();
 //! }
 //! writer.finalize().unwrap();
 //! ```
